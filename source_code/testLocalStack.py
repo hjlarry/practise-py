3f868782e812541b7497ed802906cad57e085095
hjlarry <hjlarry@163.com>
2018-09-05 14:20:10 +0800

    add appcontext localstack tests

--- source_code/testLocalStack.py
+++ source_code/testLocalStack.py
@@ -1,4 +1,4 @@
-from .FlaskOrigin import Flask, _request_ctx_stack, redirect, url_for
+from FlaskOrigin import Flask, _request_ctx_stack, redirect, url_for
 from werkzeug.wsgi import DispatcherMiddleware
 from werkzeug.serving import run_simple
 import time
@@ -49,4 +49,37 @@ run_simple(hostname='127.0.0.1', port=5002, application=application, threaded=Tr
 127.0.0.1 - - [05/Sep/2018 13:20:49] "GET / HTTP/1.1" 200 -
 {<greenlet.greenlet object at 0x11052baf8>: {'stack': [<flask_me._RequestContext object at 0x1106ab198>]}} 4
 127.0.0.1 - - [05/Sep/2018 13:20:56] "GET /admin/1 HTTP/1.1" 200 -
+'''
+
+from flask import Flask, current_app, _app_ctx_stack
+import logging
+
+app = Flask("app1")
+app2 = Flask("app2")
+
+app.config.logger = logging.getLogger("app1.logger")
+app2.config.logger = logging.getLogger("app2.logger")
+
+app.logger.addHandler(logging.FileHandler("app_log.txt"))
+app2.logger.addHandler(logging.FileHandler("app2_log.txt"))
+
+with app.app_context():
+    with app2.app_context():
+        try:
+            print(_app_ctx_stack._local.__storage__, 9)
+            raise ValueError("app2 error")
+        except Exception as e:
+            current_app.config.logger.exception(e)
+    try:
+        print(_app_ctx_stack._local.__storage__, 10)
+        raise ValueError("app1 error")
+    except Exception as e:
+        current_app.config.logger.exception(e)
+
+'''
+这段代码解决了为什么要用栈存储App Context
+
+{<greenlet.greenlet object at 0x106dee6d0>: {'stack': [<flask.ctx.AppContext object at 0x107487668>, <flask.ctx.AppContext object at 0x1074876d8>]}} 9
+Traceback (most recent call last):
+{<greenlet.greenlet object at 0x106dee6d0>: {'stack': [<flask.ctx.AppContext object at 0x107487668>]}} 10
 '''
