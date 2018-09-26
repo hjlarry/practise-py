56248c518b11db71aa21d7dc44dd2af5250e2b0e
he jiale <hjlarry@163.com>
2018-09-26 20:21:01 +0800

    add threading

--- standard_library/test.py
+++ standard_library/test.py
@@ -1,5 +1,36 @@
-import subprocess
+import logging
+import threading
+import time
 
-completed = subprocess.run(['ls', '-l'])
-print('return code:',completed.returncode)
-import signal, inspect
+def daemon():
+    logging.debug('Daemon Starting')
+    time.sleep(0.2)
+    logging.debug('Daemon Exit')
+    
+def non_daemon():
+    logging.debug('Starting')
+    logging.debug('Exit')
+    
+
+import random
+
+def worker():
+    pause = random.randint(1,5)/10
+    logging.debug('Daemon Starting sleep %0.2f', pause)
+    time.sleep(pause)
+    logging.debug('Daemon Exit')
+    
+logging.basicConfig(level=logging.DEBUG, format="[%(levelname)s] (%(threadName)-10s) %(message)s")
+for i in range(3):
+    t = threading.Thread(target=worker, daemon=True)
+    t.start()
+    
+main_thread = threading.main_thread()
+
+print(threading.enumerate())
+
+for t in threading.enumerate():
+    if t is main_thread:
+        continue
+    logging.debug('%s joining' , t.getName())
+    t.join()
