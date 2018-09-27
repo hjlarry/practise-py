0d5d8ed0f6e303c1bb359ea3c126f537e112ecfa
hjlarry <hjlarry@163.com>
2018-09-27 17:53:53 +0800

    add pipe to subprocess

--- standard_library/test.py
+++ standard_library/test.py
@@ -12,25 +12,36 @@ def non_daemon():
     logging.debug('Exit')
     
 
-import random
+# import random
+#
+# def worker():
+#     pause = random.randint(1,5)/10
+#     logging.debug('Daemon Starting sleep %0.2f', pause)
+#     time.sleep(pause)
+#     logging.debug('Daemon Exit')
+#
+# logging.basicConfig(level=logging.DEBUG, format="[%(levelname)s] (%(threadName)-10s) %(message)s")
+# for i in range(3):
+#     t = threading.Thread(target=worker, daemon=True)
+#     t.start()
+#
+# main_thread = threading.main_thread()
+#
+# print(threading.enumerate())
+#
+# for t in threading.enumerate():
+#     if t is main_thread:
+#         continue
+#     logging.debug('%s joining' , t.getName())
+#     t.join()
 
-def worker():
-    pause = random.randint(1,5)/10
-    logging.debug('Daemon Starting sleep %0.2f', pause)
-    time.sleep(pause)
-    logging.debug('Daemon Exit')
-    
-logging.basicConfig(level=logging.DEBUG, format="[%(levelname)s] (%(threadName)-10s) %(message)s")
-for i in range(3):
-    t = threading.Thread(target=worker, daemon=True)
-    t.start()
-    
-main_thread = threading.main_thread()
+import subprocess
+cat = subprocess.Popen(['cat', 'Concurrency/signal.ipynb'], stdout=subprocess.PIPE)
+grep = subprocess.Popen(['grep', 'def'],stdin=cat.stdout, stdout=subprocess.PIPE)
+cut = subprocess.Popen(['cut', '-b', '-30'],stdin=grep.stdout, stdout=subprocess.PIPE)
+end_of_pipe = cut.stdout
 
-print(threading.enumerate())
+print('Included files:')
+for line in end_of_pipe:
+    print(line)
 
-for t in threading.enumerate():
-    if t is main_thread:
-        continue
-    logging.debug('%s joining' , t.getName())
-    t.join()
