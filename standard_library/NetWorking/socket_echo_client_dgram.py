1fe3e427f29e9db7729a0e97dacd0161fcc2f2d4
hjlarry <hjlarry@163.com>
2018-10-03 00:57:55 +0800

    add udp of socket

---
+++ standard_library/NetWorking/socket_echo_client_dgram.py
@@ -0,0 +1,18 @@
+import socket
+import sys
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_addr = ("localhost", 10000)
+
+
+try:
+    message = b'this is a message ahahaha, it will repeated'
+    print("sending ", message)
+    sent = sock.sendto(message, server_addr)
+
+    data, server = sock.recvfrom(4096)
+    print('received', data)
+finally:
+    print('closing')
+    sock.close()
+
---
+++ standard_library/NetWorking/socket_echo_server_dgram.py
@@ -0,0 +1,17 @@
+import socket
+import sys
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_addr = ("localhost", 10000)
+sock.bind(server_addr)
+
+
+while True:
+    print("Wait to receive message")
+    data, client_addr = sock.recvfrom(4096)
+    print(f'received {len(data)} bytes from {client_addr}')
+    print(data)
+    if data:
+        sent = sock.sendto(data, client_addr)
+        print(f'sent {len(data)} bytes to {client_addr}')
+
