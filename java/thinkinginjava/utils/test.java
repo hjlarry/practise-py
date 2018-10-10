cef6fbc590403087b9ce51786a7827601377d997
hjlarry <hjlarry@163.com>
2018-10-10 14:23:59 +0800

    add access permission

---
+++ java/thinkinginjava/utils/test.java
@@ -0,0 +1,9 @@
+package utils;
+import ch06.*;
+public class test {
+    public static void main(String[] args){
+        Lunch ls = new Lunch();
+        int n = ls.testStatic();
+        Print.print(n);
+    }
+}
