7e884c39c032cdf26b722c2643dd3b2922f476fa
hjlarry <hjlarry@163.com>
2018-10-10 11:49:39 +0800

    add static util class

---
+++ java/thinkinginjava/ch06/PrintTest.java
@@ -0,0 +1,12 @@
+package ch06;
+
+import static utils.Print.*;
+
+public class PrintTest {
+    public static void main(String[] args){
+        print("this is test");
+        print();
+        print(100L);
+        print(100);
+    }
+}
