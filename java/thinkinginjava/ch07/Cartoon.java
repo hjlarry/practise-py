045bed1f571a4fa6f79feae5486eade9aab8367a
hjlarry <hjlarry@163.com>
2018-10-11 10:14:44 +0800

    add proxy example

--- java/thinkinginjava/ch07/Cartoon.java
+++ java/thinkinginjava/ch07/Cartoon.java
@@ -4,13 +4,14 @@ import static utils.Print.*;
 
 // 基类初始化
 class Art{
-    Art(){
+    Art(int i){
         print("this is art()");
     }
 }
 
 class  Drawing extends Art{
     Drawing(){
+        super(5);  // 必须在首行初始化父类
         print("this is drawing");
     }
 }
---
+++ java/thinkinginjava/ch07/SpaceControls.java
@@ -0,0 +1,8 @@
+package ch07;
+
+public class SpaceControls {
+    void up(int vel){}
+    void down(int vel){}
+    void left(int vel){}
+    void right(int vel){}
+}
