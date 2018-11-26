1b5534d9716aa521e991c53db48be965a2f64df9
hjlarry <hjlarry@163.com>
2018-11-26 16:17:52 +0800

    add asyncio http

---
+++ standard_library/Concurrency/asyncio/asyncio_http.py
@@ -0,0 +1,39 @@
+import socket
+from urllib.parse import urlparse
+import asyncio
+import time
+
+
+async def get_url(url):
+    url = urlparse(url)
+    host = url.netloc
+    path = url.path
+    if path == "":
+        path = "/"
+
+    reader, writer = await asyncio.open_connection(host, 80)
+    writer.write(
+        f"GET {path} HTTP/1.1\r\nHost:{host}\r\nConnection:close\r\n\r\n".encode("utf8")
+    )
+    all_lines = []
+    async for line in reader:
+        all_lines.append(line.decode("utf-8"))
+    return "\n".join(all_lines)
+
+async def main():
+    tasks = []
+    for i in range(20):
+        url = f"http://shop.projectsedu.com/goods/{i}/"
+        tasks.append(get_url(url))
+    for task in asyncio.as_completed(tasks):
+        result = await task
+        print(result)
+
+
+start = time.time()
+loop = asyncio.get_event_loop()
+try:
+    loop.run_until_complete(main())
+finally:
+    loop.close()
+    print(time.time() - start)
