a813060561b805349b3da048f46b3a5f5d91e4d9
hjlarry <hjlarry@163.com>
2018-11-30 11:09:27 +0800

    add attr search order

---
+++ source_code_research/note_of_python_learnnote/attr_search_order.py
@@ -0,0 +1,38 @@
+# 知乎文章 对象属性查找顺序的伪码
+# https://www.zhihu.com/question/25391709/answer/30634637
+
+
+def class_lookup(cls, name):
+    "Look for attribute /name/ in class /cls/ and bases."
+    v = cls.__dict__.get(name)
+    if v is not None:
+        # found in this class
+        return v, cls
+    # search in base classes
+    for i in cls.__bases__:
+        v, c = class_lookup(i, name)
+        if v is not None:
+            return v, c
+    # not found
+    return None
+
+
+def object_getattr(obj, name):
+    "Look for attribute /name/ in object /obj/."
+    # First look in class and base classes.
+    v, cls = class_lookup(obj.__class__, name)
+    if (v is not None) and hasattr(v, "__get__") and hasattr(v, "__set__"):
+        # Data descriptor.  Overrides instance member.
+        return v.__get__(obj, cls)
+    w = obj.__dict__.get(name)
+    if w is not None:
+        # Found in object
+        return w
+    if v is not None:
+        if hasattr(v, "__get__"):
+            # Function-like descriptor.
+            return v.__get__(obj, cls)
+        else:
+            # Normal data member in class
+            return v
+    raise AttributeError(obj.__class__, name)
