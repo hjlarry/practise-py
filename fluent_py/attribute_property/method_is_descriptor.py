fecda5ab056b29688d4e8fec6adef9ca0125696c
he jiale <hjlarry@163.com>
2018-12-04 21:58:33 +0800

    add method is descriptor

---
+++ fluent_py/attribute_property/method_is_descriptor.py
@@ -0,0 +1,21 @@
+import collections
+
+
+class Text(collections.UserString):
+    def __str__(self):
+        return f"Text({self.data!r})"
+
+    def reverse(self):
+        return self[::-1]
+
+
+word = Text('forward')
+print(word)
+print(word.reverse())
+print(Text.reverse(Text('backward')))
+print(type(Text.reverse), type(word.reverse))
+print(Text.reverse.__get__(word))
+print(Text.reverse.__get__(None, Text))
+print(word.reverse)
+print(word.reverse.__self__)
+print(word.reverse.__func__ is Text.reverse)
