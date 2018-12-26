7dabbb7a5d119a182c2f042ae5f78e61ea1cb9ef
hjlarry <hjlarry@163.com>
2018-12-26 09:53:18 +0800

    fix array

--- adt/array_.py
+++ adt/array_.py
@@ -2,7 +2,7 @@
 class Array:
     def __init__(self, size=32):
         self._size = size
-        self._item = [None] * self._size
+        self._item = []
 
     def __getitem__(self, index):
         return self._item[index]
@@ -11,7 +11,7 @@ class Array:
         self._item[index] = value
 
     def __len__(self):
-        return self._size
+        return len(self._item)
 
     def clear(self):
         for i in range(len(self)):
@@ -21,13 +21,46 @@ class Array:
         for item in self._item:
             yield item
 
+    def find(self, index):
+        try:
+            return self._item[index]
+        except IndexError:
+            return None
+
+    def delete(self, index):
+        try:
+            self._item.pop(index)
+            return True
+        except IndexError:
+            return False
+
+    def insert(self, index, value):
+        if len(self) >= self._size:
+            return False
+        else:
+            return self._item.insert(index, value)
+
+    def print_all(self):
+        for item in self:
+            print(item)
+
 
 def test_array():
-    a = Array()
-    assert len(a) == 32
+    array = Array(5)
+    array.insert(0, 3)
+    array.insert(0, 4)
+    array.insert(1, 5)
+    array.insert(3, 9)
+    array.insert(3, 10)
+    assert array.insert(0, 100) is False
+    assert len(array) == 5
+    array.print_all()
+    assert array.find(1) == 5
+    assert array.delete(4) is True
+    array.print_all()
+    array.clear()
+    assert array[3] is None
 
-    a[3] = 5
-    assert a[3] == 5
 
-    a.clear()
-    assert a[3] is None
+if __name__ == "__main__":
+    test_array()
