9ddbfe8119cd7ccb871a24fec6adb7bb96ecc41f
he jiale <hjlarry@163.com>
2018-12-31 13:51:46 +0800

    fix binary search

--- adt/alg/binarySearch.py
+++ adt/alg/binarySearch.py
@@ -1,42 +1,19 @@
-# 方法一：自己想的实现
-def binSearch(data, value, pos=None, direction=1):
-    mid = len(data) // 2
-    if not pos:
-        pos = list()
-    if direction:
-        pos.append(mid)
-    else:
-        pos.append(-mid)
+def binarySearch(array, t):
+    low = 0
+    height = len(array) - 1
+    while low <= height:
+        mid = (low + height) // 2
+        if array[mid] < t:
+            low = mid + 1
 
-    if data[0] == value:
-        return 0
+        elif array[mid] > t:
+            height = mid - 1
 
-    if len(data) == 1:
-        return False
+        else:
+            return mid
 
-    if data[mid] == value:
-        return sum(pos)
-    elif data[mid] > value:
-        return binSearch(data[:mid], value, pos, 0)
-    elif data[mid] < value:
-        return binSearch(data[mid:], value, pos)
+    return -1
 
-
-# def BinarySearch(array, t):
-#     low = 0
-#     height = len(array) - 1
-#     while low < height:
-#         mid = (low + height) // 2
-#         if array[mid] < t:
-#             low = mid + 1
-#
-#         elif array[mid] > t:
-#             height = mid - 1
-#
-#         else:
-#             return mid
-#
-#     return -1
 
 # 方法二
 def binary_search_recursive(sorted_array, beg, end, val):
@@ -56,12 +33,16 @@ def binary_search_recursive(sorted_array, beg, end, val):
 def test_bin_search():
     a = [10, 30, 40, 70, 100, 120, 130, 190, 200]
     print(binary_search_recursive(a, 0, len(a), 200))
-    assert binSearch(a, 10) == 0
-    assert binSearch(a, 200) == 8
-    assert binSearch(a, 130) == 6
-    assert binSearch(a, 135) is False
+    assert binarySearch(a, 10) == 0
+    assert binarySearch(a, 200) == 8
+    assert binarySearch(a, 130) == 6
+    assert binarySearch(a, 135) == -1
 
     assert binary_search_recursive(a, 0, len(a), 10) == 0
     assert binary_search_recursive(a, 0, len(a), 200) == 8
     assert binary_search_recursive(a, 0, len(a), 130) == 6
     assert binary_search_recursive(a, 0, len(a), 135) is False
+
+
+if __name__ == "__main__":
+    test_bin_search()
