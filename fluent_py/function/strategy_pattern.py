6aed20b4f1654e39fd76856c504c9c2ccbf002de
he jiale <hjlarry@163.com>
2019-03-02 21:57:11 +0800

    add strategy pattern by func

--- fluent_py/function/strategy_pattern.py
+++ fluent_py/function/strategy_pattern.py
@@ -1,4 +1,4 @@
-# 使用func实现策略模式
+# 策略模式
 from abc import ABC, abstractmethod
 from collections import namedtuple
 
@@ -29,14 +29,17 @@ class Order:
     def due(self):
         if self.promotion is None:
             discount = 0
-        else:
+        elif isinstance(self.promotion, Promotion):  # 对应类实现的策略模式
             discount = self.promotion.discount(self)
+        else:
+            discount = self.promotion(self)  # 对应函数实现的策略模式
         return self.total() - discount
 
     def __repr__(self):
         return f"<Order total: {self.total()}, due: {self.due()}>"
 
 
+# 使用类实现策略模式
 class Promotion(ABC):
     @abstractmethod
     def discount(self, order):
@@ -81,3 +84,31 @@ banana_cart = [LineItem("banana", 30, .5), LineItem("apple", 10, 1.5)]
 print(Order(joe, banana_cart, BulkItemPromo()))
 long_order = [LineItem(str(item_code), 1, 1.0) for item_code in range(10)]
 print(Order(joe, long_order, LargeOrderPromo()))
+
+
+# 使用函数实现策略模式
+def fidelity_promo(order):
+    return order.total() * .05 if order.customer.fidelity >= 1000 else 0
+
+
+def bulk_item_promo(order):
+    discount = 0
+    for item in order.cart:
+        if item.quantity >= 20:
+            discount += item.total() * .1
+    return discount
+
+
+def large_order_promo(order):
+    distinct_items = {item.product for item in order.cart}
+    if len(distinct_items) >= 10:
+        return order.total() * .07
+    return 0
+
+
+print(Order(joe, cart, fidelity_promo))
+print(Order(ann, cart, fidelity_promo))
+banana_cart = [LineItem("banana", 30, .5), LineItem("apple", 10, 1.5)]
+print(Order(joe, banana_cart, bulk_item_promo))
+long_order = [LineItem(str(item_code), 1, 1.0) for item_code in range(10)]
+print(Order(joe, long_order, large_order_promo))
