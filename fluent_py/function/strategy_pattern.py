c93d60ec2261643fd5f508a24f297c855ede0c52
he jiale <hjlarry@163.com>
2019-03-03 09:46:43 +0800

    add best promo

--- fluent_py/function/strategy_pattern.py
+++ fluent_py/function/strategy_pattern.py
@@ -112,3 +112,24 @@ banana_cart = [LineItem("banana", 30, .5), LineItem("apple", 10, 1.5)]
 print(Order(joe, banana_cart, bulk_item_promo))
 long_order = [LineItem(str(item_code), 1, 1.0) for item_code in range(10)]
 print(Order(joe, long_order, large_order_promo))
+
+
+# promos = [fidelity_promo, bulk_item_promo, large_order_promo]
+# 内省全局的promos
+promos = [
+    globals()[name]
+    for name in globals()
+    if name.endswith("_promo") and name != "best_promo"
+]
+# 也可内省单独的promotions模块
+# promps = [func for name, func in inspect.getmembers(promotions, inspect.isfunction)]
+
+
+def best_promo(order):
+    # 最佳策略
+    return max(promo(order) for promo in promos)
+
+
+print(Order(joe, long_order, best_promo))
+print(Order(joe, banana_cart, best_promo))
+print(Order(ann, cart, best_promo))
