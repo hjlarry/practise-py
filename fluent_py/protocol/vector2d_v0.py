bf5a3dcb55b05a88cfa69ba7567c2486adc1bd77
he jiale <hjlarry@163.com>
2019-03-09 19:59:06 +0800

    add vector v0 for protocol

---
+++ fluent_py/protocol/vector2d_v0.py
@@ -0,0 +1,47 @@
+from array import array
+import math
+
+
+class Vector2d:
+    type_code = "d"
+
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
+
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return "{}({!r},{!r})".format(class_name, *self)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return bytes([ord(self.type_code)]) + bytes(array(self.type_code, self))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+
+v1 = Vector2d(3, 4)
+print(v1)
+print(repr(v1))
+x, y = v1
+print(x)
+print(y)
+v1_clone = eval(repr(v1))
+print(v1 == v1_clone)
+octets = bytes(v1)
+print(octets)
+print(abs(v1))
+print(bool(v1))
+print(bool(Vector2d(0, 0)))
