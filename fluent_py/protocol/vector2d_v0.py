35ca2e4645287696eac2eb38191793a24a651265
he jiale <hjlarry@163.com>
2019-03-09 22:01:07 +0800

    add format and hash to vector2d

--- fluent_py/protocol/vector2d_v0.py
+++ fluent_py/protocol/vector2d_v0.py
@@ -6,8 +6,16 @@ class Vector2d:
     type_code = "d"
 
     def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+        self.__x = float(x)
+        self.__y = float(y)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
 
     def __iter__(self):
         return (i for i in (self.x, self.y))
@@ -31,6 +39,31 @@ class Vector2d:
     def __bool__(self):
         return bool(abs(self))
 
+    @classmethod
+    def frombytes(cls, octets):
+        type_code = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(type_code)
+        return cls(*memv)
+
+    def angle(self):
+        # 角度
+        return math.atan2(self.y, self.x)
+
+    def __format__(self, format_spec=""):
+        # 如果以p结尾，那么在极坐标中显示向量
+        if format_spec.endswith("p"):
+            format_spec = format_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_fmt = "<{}, {}>"
+        else:
+            coords = self
+            outer_fmt = "({}, {})"
+        components = (format(c, format_spec) for c in coords)
+        return outer_fmt.format(*components)
+
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
+
 
 v1 = Vector2d(3, 4)
 print(v1)
@@ -45,3 +78,9 @@ print(octets)
 print(abs(v1))
 print(bool(v1))
 print(bool(Vector2d(0, 0)))
+print(format(v1))
+print(format(v1, ".2f"))
+print(format(v1, "p"))
+print(format(v1, ".3ep"))
+print(hash(v1))
+print(set([v1]))
