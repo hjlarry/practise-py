e19f85bee25df5b3957d6f5d3d5af1644b541346
hjlarry <hjlarry@163.com>
2019-04-08 10:42:59 +0800

    clear defaultdict

--- standard_library/DataStructures/collections_ex/defaultdict.py
+++ standard_library/DataStructures/collections_ex/defaultdict.py
@@ -1,5 +1,7 @@
 import collections
 
+print("示例一、使用函数创建默认值")
+
 
 def default_factory():
     return "default value"
@@ -9,3 +11,12 @@ d = collections.defaultdict(default_factory, foo="bar")
 print(d)
 print(d["foo"])
 print(d["hello"])
+
+
+print("示例二")
+
+d = collections.defaultdict(set)
+d['a'].add(1)
+d['a'].add(2)
+d['b'].add(4)
+print(d)
