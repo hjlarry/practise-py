ee031f88bf9d8866257549de2d1fb6920caf53e2
hjlarry <hjlarry@163.com>
2020-03-10 15:13:52 +0800

    add sudoku

---
+++ small_programe/sudoku/solving_sudoku.py
@@ -0,0 +1,115 @@
+import itertools
+from sudoku import print_answers
+
+def is_full(challange, m=3):
+    for i, j in itertools.product(range(m ** 2), repeat=2):
+        if challange[i][j] is None:
+            return False
+    return True
+
+
+def cal_candidate(challange, x, y, m=3):
+
+    candidate = list(range(1, m ** 2 + 1))
+    for i in range(m ** 2):
+        # 确认一行没有重复数字
+        if challange[x][i] in candidate:
+            candidate.remove(challange[x][i])
+        # 确认一列没有重复数字
+        if challange[i][y] in candidate:
+            candidate.remove(challange[i][y])
+    # 确认一个区块(3*3)没有重复数字
+    for i, j in itertools.product(range(m), repeat=2):
+        # x0, y0 分别代表了空格所处区块的位置
+        x0, y0 = x - x % m, y - y % m
+        if challange[x0 + i][y0 + j] in candidate:
+            candidate.remove(challange[x0 + i][y0 + j])
+    return candidate
+
+
+def least_candidate(challange, m=3):
+    least, x, y = m ** 2, -1, -1
+    for i, j in itertools.product(range(m ** 2), repeat=2):
+        if not challange[i][j]:
+            num = len(cal_candidate(challange, i, j))
+            if num < least:
+                least = num
+                x, y = i, j
+    return x, y
+
+
+def solving_soduku(challange, m=3):
+    # 如果数独列表已满，返回列表
+    if is_full(challange):
+        return challange
+
+    # 找到填入数字最少的空格位置
+    x, y = least_candidate(challange)
+
+    # 为了方便递归，我们用id来表示每个空格
+    id = x * (m ** 2) + y
+
+    # try_candidate函数是我们的递归函数
+    result = try_candidate(challange, id)
+    return result
+
+
+def try_candidate(challange, id, m=3):
+    # 首先是基本结束条件
+    if is_full(challange):
+        return challange
+
+    # 根据id解析出x,y轴坐标
+    x = id // (m ** 2)
+    y = id % (m ** 2)
+
+    # 循环判断当前空格是否为None
+    while challange[x][y]:
+        # 注意,变量id可能越界
+        # 所以需要取模运算来防止越界
+        id = (id + 1) % m ** 4
+        x = id // (m ** 2)
+        y = id % (m ** 2)
+    candidate = cal_candidate(challange, x, y)
+
+    # 基本返回条件1
+    if len(candidate) == 0:
+        return False
+
+    # 顺序选择数字
+    # 并向下递归
+    for i in range(len(candidate)):
+        challange[x][y] = candidate[i]
+
+        # 自身的数字填好了
+        # 准备向下一个空格递归
+        result_r = try_candidate(challange, (id + 1) % m ** 4)
+        if not result_r:
+            # 如果后面的空格
+            # 返回None
+            # 那么我们继续选择别的数
+            pass
+        else:
+            # 后面的空格说OK!
+            # 很好我们也说OK
+            return challange
+
+    # 注意，如果程序运行到了这一步
+    # 说明没有合适的数字可以选择
+    # 需要把自身恢复成初始状态
+    # 即把空格中数字置为None
+    challange[x][y] = None
+    return False
+
+
+testing = [[None,None,None,3,None,6,None,None,None],
+           [None,1,None,8,None,None,None,None,9],
+           [5, 6,None,None,4,None,8,3,None],
+           [None,None,8,None,None,None,None,2,None],
+           [None,None,None,7,6,None,None,None,None],
+           [4,None,None,None,None,None,5,None,None],
+           [6,None,2,4,None,None,None,9,1],
+           [None,None,None,None,2,9,None,None,None],
+           [None,8,None,None,1,None,None,None,6]]
+result = solving_soduku(testing)
+print_answers(result)
---
+++ small_programe/sudoku/sudoku.py
@@ -0,0 +1,89 @@
+import random
+import itertools
+from copy import deepcopy
+
+
+def make_board(m=3):
+    # 默认数独的每一个区块是3 * 3的
+    numbers = list(range(1, m ** 2 + 1))
+    # 这里我们创建了一个含有所有可能出现数的列表（1~9）
+
+    board = None
+    # board是我们的数独二维列表
+    while board is None:
+        board = attempt_board(m, numbers)
+    return board
+
+
+def attempt_board(m, numbers):
+    n = m ** 2
+
+    board = [[None for _ in range(n)] for _ in range(n)]
+
+    for i, j in itertools.product(range(n), repeat=2):
+        # i,j分别代表的是我们的行和列
+        # i0和j0代表的是board[i][j]所在的区块的起始位置
+        i0, j0 = i - i % m, j - j % m
+
+        random.shuffle(numbers)
+        for x in numbers:
+            # 分别检查行，列，区块
+            if (
+                x not in board[i]
+                and all(row[j] != x for row in board)
+                and all(x not in row[j0 : j0 + m] for row in board[i0:i])
+            ):
+
+                # 如果检查没有问题，就开始赋值
+                board[i][j] = x
+                break
+        # 注意这个else的位置，是for...else...的语法结构
+        else:
+            return None
+    return board
+
+
+def print_board(board, m=3):
+    numbers = list(range(1, m ** 2 + 1))
+
+    # 每一行随机把5个数字变成None
+    omit = 5  # omit变量掌控着每一行被抹去的数字个数
+    challange = deepcopy(board)
+    for i, j in itertools.product(range(omit), range(m ** 2)):
+        x = random.choice(numbers) - 1
+        challange[x][j] = None
+
+    # 打印出整个数独列表
+    spacer = "++-----+-----+-----++-----+-----+-----++-----+-----+-----++"
+    print(spacer.replace("-", "="))
+    for i, line in enumerate(challange):
+        print(
+            "||  {}  |  {}  |  {}  ||  {}  |  {}  |  {}  ||  {}  |  {}  |  {}  ||".format(
+                *(cell or " " for cell in line)
+            )
+        )
+        if (i + 1) % 3 == 0:
+            print(spacer.replace("-", "="))
+        else:
+            print(spacer)
+    return challange
+
+
+def print_answers(board):
+    spacer = "++-----+-----+-----++-----+-----+-----++-----+-----+-----++"
+    print(spacer.replace("-", "="))
+    for i, line in enumerate(board):
+        print(
+            "||  {}  |  {}  |  {}  ||  {}  |  {}  |  {}  ||  {}  |  {}  |  {}  ||".format(
+                *(cell or " " for cell in line)
+            )
+        )
+        if (i + 1) % 3 == 0:
+            print(spacer.replace("-", "="))
+        else:
+            print(spacer)
+
+if __name__ == "__main__":
+    board = make_board()
+    print_board(board)
+    print_answers(board)
