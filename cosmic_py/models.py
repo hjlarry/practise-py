e82372f12d9d1d947a7059713bac5258263ea3e5
hjlarry <hjlarry@163.com>
2020-04-29 15:06:47 +0800

    support batch allocate and deallocate orderline

--- cosmic_py/models.py
+++ cosmic_py/models.py
@@ -15,12 +15,24 @@ class Batch:
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = qty
+        self._purchased_quantity  = qty
+        self._allocations = set()
 
     def allocate(self, line: OrderLine):
-        self.available_quantity -= line.qty
+        if self.can_allocate(line):
+            self._allocations.add(line)
+    
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
 
     def can_allocate(self, line: OrderLine):
         return self.sku == line.sku and self.available_quantity >= line.qty
 
-    
+    @property
+    def allocated_quantity(self):
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self):
+        return self._purchased_quantity - self.allocated_quantity
