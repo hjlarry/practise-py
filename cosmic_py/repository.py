93be29dc429971526163f96e9e93b403aae30954
hjlarry <hjlarry@163.com>
2020-05-06 15:43:52 +0800

    add repository layer

---
+++ cosmic_py/repository.py
@@ -0,0 +1,27 @@
+import abc
+
+import models
+
+
+class AbstractRepository(abc.ABC):
+    @abc.abstractmethod
+    def add(self, batch: models.Batch):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, reference) -> models.Batch:
+        raise NotImplementedError
+
+
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, batch):
+        self.session.add(batch)
+
+    def get(self, reference):
+        return self.session.query(models.Batch).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(models.Batch).all()
