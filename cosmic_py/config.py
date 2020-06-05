d5e2ed75cf045b20d06f0c7fdc98b2c22cec445d
hjlarry <hjlarry@163.com>
2020-06-05 13:35:44 +0800

    add the redis event consumer

--- cosmic_py/config.py
+++ cosmic_py/config.py
@@ -13,3 +13,9 @@ def get_postgres_uri():
     password = os.environ.get("DB_PASSWORD", "abc123")
     user, db_name = "allocation", "allocation"
     return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
+
+def get_redis_host_and_port():
+    host = os.environ.get("REDIS_HOST", "localhost")
+    port = 63791 if host == "localhost" else 6379
+    return dict(host=host, port=port)
