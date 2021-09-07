96479f0f73a7cc879ca00e5fdf3b7b32bd1b57a5
hjlarry <hjlarry@163.com>
2021-09-07 11:44:16 +0800

    update functools.singledispatch example

--- small_programe/single_dispatch.py
+++ small_programe/single_dispatch.py
@@ -2,34 +2,59 @@ from functools import singledispatch
 from collections import abc
 import numbers
 import html
+import fractions
+import decimal
 
 
 @singledispatch
-def htmlize(obj):
+def htmlize(obj: object) -> str:
     content = html.escape(repr(obj))
     return f"<pre>{content}</pre>"
 
 
-@htmlize.register(str)
-def _(text):
+# Python 3.7以后可以直接用type hint决定 single_dispatch的类型
+@htmlize.register
+def _(text: str) -> str:
     content = html.escape(text).replace("\n", "<br>\n")
     return f"<p>{content}</p>"
 
 
-@htmlize.register(numbers.Integral)
-def _(n):
-    return f"<pre>{n} (0x{n:x})</pre>"
-
-
-@htmlize.register(tuple)
-@htmlize.register(abc.MutableSequence)
-def _(seq):
+@htmlize.register
+def _(seq: abc.Sequence) -> str:
     inner = "</li>\n<li>".join(htmlize(item) for item in seq)
     return "<ul>\n<li>" + inner + "</li>\n</ul>"
 
 
+@htmlize.register
+def _(n: numbers.Integral) -> str:
+    return f"<pre>{n} (0x{n:x})</pre>"
+
+
+@htmlize.register
+def _(n: bool) -> str:
+    return f"<pre>{n}</pre>"
+
+
+# Python 3.7之前可以用这种方式
+@htmlize.register(fractions.Fraction)
+def _(x) -> str:
+    frac = fractions.Fraction(x)
+    return f"<pre>{frac.numerator}/{frac.denominator}</pre>"
+
+
+@htmlize.register(decimal.Decimal)
+@htmlize.register(float)
+def _(x) -> str:
+    frac = fractions.Fraction(x).limit_denominator()
+    return f"<pre>{x} ({frac.numerator}/{frac.denominator})</pre>"
+
+
 print(htmlize({1, 2, 3}))
 print(htmlize(abs))
-print(htmlize(42))
 print(htmlize("Heimlich & Co. \n- a game"))
+print(htmlize(42))
 print(htmlize(["alpha", 66, {3, 2, 2}]))
+print(htmlize(True))
+print(htmlize(2 / 3))
+print(htmlize(fractions.Fraction(2, 3)))
+print(htmlize(decimal.Decimal("0.232826")))
