8d79b385de58fcaf160b26876cf0fc2c58abf468
hjlarry <hjlarry@163.com>
2021-11-26 15:31:57 +0800

    update spinner async example

--- fluent_py/asyncio_pkg/spinner_async.py
+++ fluent_py/asyncio_pkg/spinner_async.py
@@ -1,40 +1,37 @@
 import asyncio
 import itertools
-import sys
 
 
-async def spin(msg):
-    write, flush = sys.stdout.write, sys.stdout.flush
+async def spin(msg: str) -> None:
     for char in itertools.cycle("|/-\\"):
-        status = char + " " + msg
-        write(status)
-        flush()
-        # 使用退格符移回光标
-        write("\x08" * len(status))
+        status = f"\r{char} {msg}"
+        print(status, end="", flush=True)
         try:
             await asyncio.sleep(0.1)
         except asyncio.CancelledError:
             break
-    write(" " * len(status) + "\x08" * len(status))
+    blanks = " " * len(status)
+    print(f"\r{blanks}\r", end="")
 
 
-async def slow_function():
+async def slow_function() -> int:
     # 假装io等待
     await asyncio.sleep(3)
     return 42
 
 
-async def supervisor(loop):
-    spinner = loop.create_task(spin("thinking"))
-    print("spinner obj", spinner)
+async def supervisor() -> int:
+    spinner = asyncio.create_task(spin("thinking"))
+    print(f"spinner obj: {spinner}")
     result = await slow_function()
     spinner.cancel()
     return result
 
 
-loop = asyncio.get_event_loop()
-try:
-    result = loop.run_until_complete(supervisor(loop))
-finally:
-    loop.close()
-    print("Answer:", result)
+def main() -> None:
+    result = asyncio.run(supervisor())
+    print(f"Answer: {result}")
+
+
+if __name__ == "__main__":
+    main()
