a84aa6422a382c67a158db4e770afc9bc4e07fea
hjlarry <hjlarry@163.com>
2021-11-29 11:07:23 +0800

    add prime numbers sequential single thread

--- fluent_py/asyncio_pkg/primes.py
+++ fluent_py/asyncio_pkg/primes.py
@@ -2,6 +2,32 @@ import math
 import asyncio
 
 
+PRIME_FIXTURE = [
+    (2, True),
+    (142702110479723, True),
+    (299593572317531, True),
+    (3333333333333301, True),
+    (3333333333333333, False),
+    (3333335652092209, False),
+    (4444444444444423, True),
+    (4444444444444444, False),
+    (4444444488888889, False),
+    (5555553133149889, False),
+    (5555555555555503, True),
+    (5555555555555555, False),
+    (6666666666666666, False),
+    (6666666666666719, True),
+    (6666667141414921, False),
+    (7777777536340681, False),
+    (7777777777777753, True),
+    (7777777777777777, False),
+    (9999999999999917, True),
+    (9999999999999999, False),
+]
+
+NUMBERS = [n for n, _ in PRIME_FIXTURE]
+
+
 def is_prime(n: int) -> bool:
     if n < 2:
         return False
@@ -32,3 +58,10 @@ async def is_prime_nap(n: int) -> bool:
         if i % 100_000 == 1:
             await asyncio.sleep(0)
     return True
+
+
+if __name__ == "__main__":
+    for n, prime in PRIME_FIXTURE:
+        prime_res = is_prime(n)
+        assert prime_res == prime
+        print(n, prime)
