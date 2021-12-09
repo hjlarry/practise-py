73878d42fd989b7092cf0786ae5cfb8079a64db4
hjlarry <hjlarry@163.com>
2021-12-09 16:02:36 +0800

    update flags asyncio 3rd version

--- fluent_py/concurrency/flags3_asyncio.py
+++ fluent_py/concurrency/flags3_asyncio.py
@@ -1,12 +1,12 @@
 # 与flags2_asyncio.py区别是每次下载国旗时发起多次请求
-
-import collections
+from collections import Counter
 import asyncio
+from http import HTTPStatus
 
-import aiohttp
+import httpx
 import tqdm
 
-from flags2_common import main, HTTPStatus, Result, save_flag
+from flags2_common import main, DownloadStatus, Result, save_flag
 
 
 DEFAULT_CONCUR_REQ = 5
@@ -18,88 +18,83 @@ class FetchError(Exception):
         self.country_code = country_code
 
 
-async def http_get(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            if resp.status == 200:
-                ctype = resp.headers.get("Content-type", "").lower()
-                if "json" in ctype or url.endswith("json"):
-                    data = await resp.json()
-                else:
-                    data = await resp.read()
-                return data
-            elif resp.status == 404:
-                raise aiohttp.web.HTTPNotFound()
-            else:
-                raise aiohttp.http.HttpProcessingError(
-                    code=resp.code, message=resp.reason, headers=resp.headers
-                )
-
-
-async def get_country(base_url, cc):
-    url = f"{base_url}/{cc.lower()}/metadata.json"
-    metadata = await http_get(url)
+async def get_country(client: httpx.AsyncClient, base_url: str, cc: str) -> str:
+    url = f"{base_url}/{cc}/metadata.json"
+    resp = await client.get(url, timeout=3.1, follow_redirects=True)
+    resp.raise_for_status()
+    metadata = resp.json()
     return metadata["country"]
 
 
-async def get_flag(base_url, cc):
-    url = f"{base_url}/{cc.lower()}/{cc.lower()}.gif"
-    return await http_get(url)
+async def get_flag(client: httpx.AsyncClient, base_url: str, cc: str) -> bytes:
+    url = f"{base_url}/{cc}/{cc}.gif".lower()
+    resp = await client.get(url, timeout=6.1, follow_redirects=True)
+    resp.raise_for_status()
+    return resp.read()
 
 
-async def download_one(cc, base_url, semaphore, verbose=False):
+async def download_one(
+    client: httpx.AsyncClient,
+    cc: str,
+    base_url: str,
+    semaphore: asyncio.Semaphore,
+    verbose: bool,
+):
     try:
-        with (await semaphore):
-            image = await get_flag(base_url, cc)
-        with (await semaphore):
-            country = await get_country(base_url, cc)
-    except aiohttp.web.HTTPNotFound:
-        status = HTTPStatus.not_found
-        msg = "not found"
-    except Exception as exc:
-        raise FetchError(cc) from exc
+        async with semaphore:
+            image = await get_flag(client, base_url, cc)
+        async with semaphore:
+            country = await get_country(client, base_url, cc)
+    except httpx.HTTPStatusError as exc:
+        res = exc.response
+        if res.status_code == HTTPStatus.NOT_FOUND:
+            status = DownloadStatus.not_found
+            msg = f"not found {res.url}"
+        else:
+            raise
     else:
-        country = country.replace(" ", "_")
-        filename = f"{country}-{cc}.gif"
-        loop = asyncio.get_event_loop()
-        loop.run_in_executor(None, save_flag, image, filename)
-        status = HTTPStatus.ok
+        filename = country.replace(" ", "_")
+        await asyncio.to_thread(save_flag, image, f"{filename}.gif")
+        status = DownloadStatus.ok
         msg = "OK"
+
     if verbose and msg:
         print(cc, msg)
     return Result(status, cc)
 
 
-async def downloader_coro(cc_list, base_url, verbose, concur_req):
-    counter = collections.Counter()
+async def supervisor(cc_list: list[str], base_url: str, verbose: bool, concur_req: int):
+    counter: Counter[DownloadStatus] = Counter()
     semaphore = asyncio.Semaphore(concur_req)
-    to_do = [download_one(cc, base_url, semaphore, verbose) for cc in sorted(cc_list)]
-    to_do_iter = asyncio.as_completed(to_do)
-    if not verbose:
-        to_do_iter = tqdm.tqdm(to_do_iter, total=len(cc_list))
-    for future in to_do_iter:
-        try:
-            res = await future
-        except FetchError as exc:
-            country_code = exc.country_code
+    async with httpx.AsyncClient() as client:
+        to_do = [
+            download_one(client, cc, base_url, semaphore, verbose)
+            for cc in sorted(cc_list)
+        ]
+        to_do_iter = asyncio.as_completed(to_do)
+        if not verbose:
+            to_do_iter = tqdm.tqdm(to_do_iter, total=len(cc_list))
+        for coro in to_do_iter:
             try:
-                error_msg = exc.__cause__.args[0]
-            except IndexError:
-                error_msg = exc.__cause__.__class__.__name__
-            if verbose and error_msg:
-                print(f"*** Error for {country_code}: {error_msg}")
-            status = HTTPStatus.error
-        else:
-            status = res.status
-        counter[status] += 1
+                res = await coro
+            except FetchError as exc:
+                country_code = exc.country_code
+                try:
+                    error_msg = exc.__cause__.message  # type:ignore
+                except AttributeError:
+                    error_msg = "Unknown Cause"
+                if verbose and error_msg:
+                    print(f"*** Error for {country_code}: {error_msg}")
+                status = DownloadStatus.error
+            else:
+                status = res.status
+            counter[status] += 1
     return counter
 
 
-def download_many(cc_list, base_url, verbose, concur_req):
-    loop = asyncio.get_event_loop()
-    coro = downloader_coro(cc_list, base_url, verbose, concur_req)
-    counts = loop.run_until_complete(coro)
-    loop.close()
+def download_many(cc_list: list[str], base_url: str, verbose: bool, concur_req: int):
+    coro = supervisor(cc_list, base_url, verbose, concur_req)
+    counts = asyncio.run(coro)
     return counts
 
 
