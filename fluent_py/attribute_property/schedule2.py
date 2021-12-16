538bc488e6c369acdd2c651df3f2eba444aea24a
hjlarry <hjlarry@163.com>
2021-12-16 15:15:48 +0800

    update schedule2 example

--- fluent_py/attribute_property/schedule2.py
+++ fluent_py/attribute_property/schedule2.py
@@ -1,105 +1,61 @@
-import warnings
-import osconfeed
-import shelve
+import json
 import inspect
 
-DB_NAME = "schedule2_db"
+
+JSON_PATH = "osconfeed.json"
 
 
 class Record:
+    __index = None
+
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
-    def __eq__(self, other):
-        if isinstance(other, Record):
-            return self.__dict__ == other.__dict__
-        else:
-            return NotImplemented
-
-
-class DbRecord(Record):
-    __db = None
-
-    @staticmethod
-    def set_db(db):
-        DbRecord.__db = db
-
-    @staticmethod
-    def get_db():
-        return DbRecord.__db
-
-    @classmethod
-    def fetch(cls, ident):
-        db = cls.get_db()
-        try:
-            return db[ident]
-        except TypeError:
-            if db is None:
-                raise RuntimeError("missing to set db")
-            else:
-                raise
-
     def __repr__(self):
-        if hasattr(self, "serial"):
-            return f"<{self.__class__.__name__} serial={self.serial !r}>"
+        cls_name = self.__class__.__name__
+        return f"<{cls_name}  serial={self.serial!r}>"
+
+    @staticmethod
+    def fetch(key):
+        if Record.__index is None:
+            Record.__index = load()
+        return Record.__index[key]
+
+
+class Event(Record):
+    def __repr__(self):
+        if hasattr(self, "name"):
+            return f"<{self.__class__.__name__} {self.name!r}>"
         else:
             return super().__repr__()
 
-
-class Event(DbRecord):
     @property
     def venue(self):
         key = f"venue.{self.venue_serial}"
         return self.__class__.fetch(key)
 
-    @property
-    def speakers(self):
-        if not hasattr(self, "_speaker_objs"):
-            spkr_serials = self.__dict__["speakers"]
-            self._speaker_objs = [
-                self.__class__.fetch(f"speaker.{key}") for key in spkr_serials
-            ]
-        return self._speaker_objs
 
-    def __repr__(self):
-        if hasattr(self, "name"):
-            return f"<{self.__class__.__name__} name={self.name !r}>"
-        else:
-            return super().__repr__()
-
-
-def load_db(db):
-    raw_data = osconfeed.load()
-    warnings.warn("loading " + DB_NAME)
-    for collection, rec_list in raw_data["Schedule"].items():
+def load(path=JSON_PATH):
+    records = {}
+    with open(path) as fp:
+        raw_data = json.load(fp)
+    for collection, raw_records in raw_data["Schedule"].items():
         record_type = collection[:-1]
         cls_name = record_type.capitalize()
-        cls = globals().get(cls_name, DbRecord)
-        # 确定cls是一个类，且可能是DbRecord的任一子类
-        if inspect.isclass(cls) and issubclass(cls, DbRecord):
+        cls = globals().get(cls_name, Record)
+        if inspect.isclass(cls) and issubclass(cls, Record):
             factory = cls
         else:
-            factory = DbRecord
-        for record in rec_list:
-            key = f"{record_type}.{record['serial']}"
-            record["serial"] = key
-            db[key] = factory(**record)
+            factory = Record
+        for raw_record in raw_records:
+            key = f"{record_type}.{raw_record['serial']}"
+            records[key] = factory(**raw_record)
+    return records
 
 
-db = shelve.open(DB_NAME)
-# 判断数据库是否填充的一个方法
-if "conference.115" not in db:
-    load_db(db)
-
-DbRecord.set_db(db)
-event = DbRecord.fetch("event.33950")
-print(event)
-print(event.venue)
-print(event.venue.name)
-print(event.speakers)
-
-speaker = DbRecord.fetch("speaker.3471")
-print(speaker)
-print(speaker.name)
-print(speaker.twitter)
-db.close()
+if __name__ == "__main__":
+    event = Record.fetch("event.33950")
+    print(event)
+    print(event.venue)
+    print(event.venue.name)
+    print(event.venue_serial)
